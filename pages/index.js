b9f0403835b1bc3e682750fd40fe709907240e9a
viktorleite <vitorlesantos@outlook.com>
2021-04-25 08:56:50 -0400

    Alterando a index.js

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,19 @@
-import Styles from './index.module.css'
 import Link from 'next/link'
 
-const sendMessage = (number, text) => {
-  const uri = `https://api.whatsapp.com/send?phone=${number}&text=${text}`
-  return uri
-}
-
 export default function Home() {
-  const profile = sendMessage(5567991629233, "Leite, ta funcionando")
-  
-  return (
+ return (
   <>
     <head>
       <title> Viktor Leite | Blog</title>
     </head>
-    <div className={Styles.container}>
-     <div class="info">
-        <h3 className={Styles.title}>Viktor Leite</h3>
-        
-        <div className={Styles.backToHome}>
-          <Link href={profile}>
-            <a>
-              <span>Entrar em contato</span>
-            </a>
-          </Link>
+    <section>
+      <div class="container">
+        <div class="info">
+          <h1>Hi, I am 
+            <span class="red"> Viktor Leite</span>.</h1>
         </div>
-     </div>
-    </div>
+      </div>
+    </section>
   </>
   )
 }
