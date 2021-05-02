a557528183954ae933ac64b59f855e65508c4cbc
viktorleite <vitorlesantos@outlook.com>
2021-05-02 18:45:03 -0400

    Melhorando a Pagina Inicial

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,32 @@ export default function Home() {
  return (
   <>
     <head>
-      <title> Viktor Leite | Blog</title>
+      <title> Viktor Leite</title>
     </head>
+   
+    <header>
+        <div class="container">
+        <div class="nav">
+          <ul>
+            <li><a href="" class="bt-contact"><span>Entrar em Contato</span></a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    
     <section>
       <div class="container">
         <div class="info">
-          <h1>Hi, I am 
-            <span class="red"> Viktor Leite</span>.</h1>
+          <h1>Hi, I'm Viktor Leite</h1>
+          <p> Front-End Developer from in Campo Grande,
+          Brazil</p>
         </div>
       </div>
     </section>
+    <footer>
+         <p>© Viktor Leite</p>
+    </footer>
   </>
+
   )
 }
